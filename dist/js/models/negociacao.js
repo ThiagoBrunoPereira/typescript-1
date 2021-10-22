716ed7e7214780a0c53c17dcc5ac14472033fb47
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2021-10-22 10:29:24 -0300

    first commit

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,19 @@
+export class Negociacao {
+    constructor(data, quantidade, valor) {
+        this.privete_data = data;
+        this.privete_quantidade = quantidade;
+        this.privete_valor = valor;
+    }
+    get data() {
+        return this.privete_data;
+    }
+    get quantidade() {
+        return this.privete_quantidade;
+    }
+    get valor() {
+        return this.privete_valor;
+    }
+    get volume() {
+        return this.privete_quantidade * this.privete_valor;
+    }
+}
